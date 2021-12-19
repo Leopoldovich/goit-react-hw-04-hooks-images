c6ca01545aef3dc2a29d4c42c9350b5840be7eb3
Leopoldovich <levchenkobohdan@gmail.com>
2021-12-19 17:28:46 +0200

    tel1

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,45 @@
+import { useEffect } from 'react'
+import { createPortal } from 'react-dom'
+import PropTypes from 'prop-types'
+import s from './Modal.module.css'
+
+const root = document.querySelector('#root-modal')
+
+export default function Modal({ item, onCloseModal }) {
+  const onCloseKey = (e) => {
+    if (e.code === 'Escape') {
+      onCloseModal()
+    }
+  }
+
+  const onCloseBackdrop = (e) => {
+    if (e.target === e.currentTarget) {
+      onCloseModal()
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', onCloseKey)
+    return () => {
+      window.removeEventListener('keydown', onCloseKey)
+    }
+  }, [])
+
+  const { src, tags } = item
+  return createPortal(
+    <div className={s.overlay} onClick={onCloseBackdrop}>
+      <div className={s.modal}>
+        <img src={src} alt={tags} />
+      </div>
+    </div>,
+    root,
+  )
+}
+
+Modal.propTypes = {
+  item: PropTypes.shape({
+    src: PropTypes.string.isRequired,
+    tags: PropTypes.string.isRequired,
+  }),
+  onCloseModal: PropTypes.func.isRequired,
+}
